9f4ecc2ca7aa4b205ca3c0b6be660c8405be9225
ZouFitahiana <129894768+Fitahiana-herizo-RAKOTOMAMPIONONA@users.noreply.github.com>
2024-12-06 19:52:55 +0300

    Accueil avec Card

--- src/components/navbar/nav.bar.jsx
+++ src/components/navbar/nav.bar.jsx
@@ -1,19 +1,23 @@
+import { useState } from "react"
 import { navListe } from "../../data/route.data"
 
-
 export default function NavBar (){
-    return <div className="flex justify-between pt-5 pb-5">
-        <div className="gauche"> Herizo</div>
-        <div className="flex gap-5">
+    const [isMenuActive ,setIsMenuActive ]  = useState(false)
+    return <div className="block sm:flex justify-between  pt-5 pb-5">
+        <div className="text-xl font-semibold"> Herizo</div>
+        <div className="block  sm:flex gap-5 hidden">
             {
                 navListe.map((item ,key)=>
                 {
-                    return <a href={item.nom != "Accueil" ? "/"+item.nom : "/"} key={key}>
-                        {item.nom} 
-                    </a>
+                    return <div key={key}>
+                        <a href={item.nom != "Accueil" ? "/"+item.nom : "/"}>
+                            {item.nom} 
+                        </a>
+                    </div>
                 })
             }
         </div>
-        <div className="gauche"> Herizo</div>
+        
+        <div className={isMenuActive ? "hidden"  : "hidden"}> Herizo</div>
     </div>
 }
--- src/routes/accueil/accueil.route.jsx
+++ src/routes/accueil/accueil.route.jsx
@@ -1,5 +1,39 @@
 export default function Accueil (){
     return <div>
-        acceuil
+        <div className="bg-slate-300 w-[100%] h-[auto] p-5 pt-10  pb-10 rounded-2xl sm:w-[50%] bg-black md: lg:">
+            <div className="w-[200px] m-auto  sm: md: lg:">
+                <img src="./assets/image/profile.jpg" alt="" srcSet="" className="w-[100%] h-[180px] object-center rounded-tl-lg rounded-br object-cover"/>
+            </div>
+            <div className="text-sm p-2 relative">
+                <div>AN DEVELOPPER</div>
+                <div className=" font-bold text-lg">Herizo RAKOTOMAMPIONONA .</div>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Qui cum aut beatae
+                </div>
+                <div className=" absolute right-2">
+                    --
+                </div>
+            </div>
+        </div>
+        <div className=" bg-black md mt-3 w-[100%] p-5 rounded-2xl sm:w-[50%]: lg:">
+            
+        </div>
+        <div className=" bg-black mt-3 w-[100%] h-[auto] p-5 pt-10  pb-10 rounded-2xl sm:w-[50%] md: lg:">
+            <div className="w-[200px] m-auto  sm: md: lg:">
+                <img src="./assets/image/profile.jpg" alt="" srcSet="" className="w-[100%] h-[180px] object-center rounded-tl-lg rounded-br object-cover"/>
+            </div>
+            <div className="text-sm p-2 relative">
+                <div>AN DEVELOPPER</div>
+                <div className=" font-bold text-lg">Herizo RAKOTOMAMPIONONA .</div>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Qui cum aut beatae
+                </div>
+                <div className=" absolute right-2">
+                    --
+                </div>
+            </div>
+        </div>
     </div>
 }
