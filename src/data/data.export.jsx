97e149ddad8bc43f782ef422aad89f3528a97c4e
ZouFitahiana <129894768+Fitahiana-herizo-RAKOTOMAMPIONONA@users.noreply.github.com>
2024-12-10 15:27:19 +0300

    clean code termine components

--- src/data/data.export.jsx
+++
@@ -1,7 +0,0 @@
-import { datacontact } from "./data.contact";
-import { experience } from "./data.experience";
-import { dataservice } from "./data.service";
-import { navListe } from "./route.data";
-import skills from "./skills.data";
-
-export {datacontact , dataservice , experience , navListe , skills}
