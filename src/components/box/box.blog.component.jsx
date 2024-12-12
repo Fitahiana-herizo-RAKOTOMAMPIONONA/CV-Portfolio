7790d2d577957469dc36ec5a9ff2e635cd6ba6f8
ZouFitahiana <129894768+Fitahiana-herizo-RAKOTOMAMPIONONA@users.noreply.github.com>
2024-12-12 15:40:19 +0300

    blog success

--- src/components/box/box.blog.component.jsx
+++ src/components/box/box.blog.component.jsx
@@ -12,14 +12,14 @@ const CardSearchResult = (props) => {
     const { search, setSearch, filteredData } = props;
 
     return (
-        <div className="col-span-1 sm:w-full">
+        <div className="col-span-1 w-full sm:w-full">
             <div className="rounded-full backgroundPers p-3 mt-2 mb-2 pl-8 flex items-center w-full relative">
                 <input
                     type="text"
                     className="p-1 border-0 bg-transparent focus:outline-none focus:ring-0"
                     placeholder="Search..."
-                    value={search} // Synchronise la valeur avec l'état global
-                    onChange={(e) => setSearch(e.target.value)} // Met à jour l'état
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
                 />
                 <button
                     className="bg-gray-400 bg-opacity-40 backdrop-blur-xl pt-1 pb-1 pl-4 pr-4 rounded-full text-white focus:outline-none focus:ring-0 absolute right-3"
