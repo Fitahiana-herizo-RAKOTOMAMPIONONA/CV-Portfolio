ab00230e23d37172edf04a19b2e91b1454d61034
ZouFitahiana <129894768+Fitahiana-herizo-RAKOTOMAMPIONONA@users.noreply.github.com>
2024-12-13 18:12:38 +0300

    grid about ok

--- src/routes/about/about.route.jsx
+++ src/routes/about/about.route.jsx
@@ -2,15 +2,17 @@ import { CardAccueil, CardDeco, CardExperience, Titre } from "../../components/c
 import { experience, formation } from "../../data/data.experience.jsx"
 
 export default function About (){
-    return <div className="grid gap-3">
-        <div className="w-[300px] p-6 backgroundPers rounded-2xl m-auto">
+    return <div className="grid gap-3 grid-cols-1 lg:grid-cols-2">
+        <div className="w-[300px] p-6 backgroundPers rounded-2xl m-auto row-span-1 lg:row-span-2">
             <img src="./assets/image/profile.jpg" alt="" srcSet="" className="w-full rounded-xl"/>
         </div>
-        <Titre title="Self summary"/>
+        <Titre title="Self summary" className={"lg:m-auto"}/>
         <CardDeco title="RAKOTOMAMPIONONA FITAHIANA" titleBlue ="Herizo." description="un etudiant qui suit la mention informatique au sein de l'ISPM"/>
-        <CardExperience title="experience" data={experience}/>
-        <CardExperience title="formation" data={formation}/>
-        <CardDeco title="Let's work" titleBlue ="together." to="/contact"/>
+        {/* <div className="sm:flex gap-3 "> */} 
+            <CardExperience title="experience" data={experience}/>
+            <CardExperience title="formation" data={formation}/>
+        {/* </div> */}
+        <CardDeco title="Let's work" titleBlue ="together." to="/contact" className2="lg:col-span-2"/>
         <CardAccueil
             description="MORE ABOUT ME" 
             title="Credentials" 
